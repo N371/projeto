2309e2e4f2ab6a97ec47958cd97d10203efb626f
Nelson Silva <73814321+N371@users.noreply.github.com>
2024-01-22 10:42:50 -0300

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -86,6 +86,20 @@ source ~/.vim/coc.nvimrc
 EOL
 
 cat <<EOL > ~/.vim/coc-settings.json
+{
+  "jedi.enable": true,
+  "jedi.startupMessage": true,
+  "jedi.markupKindPreferred": "plaintext",
+  "jedi.trace.server": true,
+  "jedi.jediSettings.autoImportModules": [],
+  "jedi.executable.command": "jedi-language-server",
+  "jedi.executable.args": [],
+  "jedi.completion.disableSnippets": false,
+  "jedi.diagnostics.enable": true,
+  "jedi.diagnostics.didOpen": true,
+  "jedi.diagnostics.didChange": true,
+  "jedi.diagnostics.didSave": true
+}
 EOL
 
 cat <<EOL > ~/.vim/coc.vimrc
