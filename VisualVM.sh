c8b5efad273168c7999c44ba33f9a23815818574
N371 <msn_nelson@hotmail.com>
2024-01-23 13:43:38 +0000

    insersao da rotinca conect para sql

--- VisualVM.sh
+++
@@ -1,5 +0,0 @@
-wget https://github.com/visualvm/visualvm.src/releases/download/2.0.7/visualvm_207.zip
-unzip visualvm_207.zip -d visualvm
-cd visualvm*
-
-
--- conect.java
+++ conect.java
@@ -1,1 +1,34 @@
-package banco_dados
+  import java.util.Scanner;
+  
+
+  public class conect{
+  	public static void main(String[] arg){
+		String head = "";
+                String usuario = "";
+ 		String senha = "";
+                String projetos = "";		
+
+        Scanner scanner = new Scanner(System.in);
+
+	System.out.println("Entre com no nome do usuario do banco");
+	usuario = scanner.next();
+	System.out.print("\033[H\033[2J");
+	System.out.println("Entre com a senha de usuario do banco");
+	senha = scanner.next();
+	System.out.print("\033[H\033[2J");
+	System.out.println("Enrre com o nome do projeot ");
+	projetos = scanner.next();
+	System.out.print("\033[H\033[2J");
+
+	head ="import java.sql.*;" + "\n" + "public class conexadoBancoDados{\n" + 
+        "Connection conBanco;" + "\n" + "public boolean abrirConexao(){" + "\n" + 
+	"String url = \"jdbc:mysql://localhost/" + projetos + "?user=" + usuario +"password="  + senha +"; \""  +
+	"\n try{ \n" + "}catch(Exception erro){erro.printStackTrace();" + "\n" + "return false;" + "\n" + "}" + "\n" + "}" + "}";
+	
+//System.out.println(head);
+         geraFile GeraFile = new geraFile("conexaoBandoCados",head);
+         GeraFile.geraFileExecute();
+            	 
+	
+	}
+  }
